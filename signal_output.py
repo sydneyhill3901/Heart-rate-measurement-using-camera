cf7190b1c7da74136471b5608146828b2e23a9ed
Sydney Hill <sydneyhill@oakland.edu>
2021-02-02 14:52:30 -0500

    Updated signal_output.py

--- signal_output.py
+++ signal_output.py
@@ -8,7 +8,7 @@ from sklearn.model_selection import train_test_split
 
 class signalWriter(object):
     def __init__(self):
-        self.dir = './videos'
+        self.dir = './test_small_real'
         self.outputPath = ""
         self.vid_count = 0
         self.process = ProcessMod(GUIMode=False)
@@ -17,15 +17,16 @@ class signalWriter(object):
         self.write_log_check = False
         self.frame_data = {}
         self.video_data = []
-        self.data = {}
+        self.real_data = {}
+        self.fake_data = {}
         self.frame_count = 0
         self.missed_frames = 0
         self.current_missed_streak = 0
         self.greatest_missed_streak = 0
+        self.real_count = 0
+        self.fake_count = 0
         self.full_path = ''
         self.log_path = self.dir + '/process_log.csv'
-        self.test_data_path = self.dir + '/train_data.csv'
-        self.train_data_path = self.dir + '/test_data.csv'
         self.output = open(self.log_path, "w+", encoding="utf8")
         self.writer = csv.DictWriter(self.output, fieldnames=['File', 'Frames missed', 'Largest missing interval', 'Percent detected'], delimiter=',', lineterminator="\r", quoting=csv.QUOTE_NONE)
         self.file_log = {
@@ -43,28 +44,30 @@ class signalWriter(object):
         self.file_log['File'] = self.full_path
         self.file_log['Frames missed'] = self.missed_frames
         self.file_log['Largest missing interval'] = self.greatest_missed_streak
-
         self.file_log['Percent detected'] = (self.frame_count - self.missed_frames)/self.frame_count
         self.writer.writerow(self.file_log)
 
-    def write_derived_features(self, dataset):
+    def write_derived_features(self):
 
-        difg_LCRC = (dataset['RC-G'] - dataset['LC-G']).abs()
-        difc_LCRC = (dataset['RC-C'] - dataset['LC-C']).abs()
-        #avgg_i = (pd.DataFrame(dataset, columns=['RC-G', 'LC-G']).rolling(10).mean()).mean(axis=0)
-        #avgc_i = (pd.DataFrame(dataset, columns=['RC-C', 'LC-C']).rolling(10).mean()).mean(axis=0)
-        difg_o = ((dataset['C-G'] - dataset['F-G']).abs() - (dataset['OR-G'] - dataset['OL-G']).abs()).abs()
-        difc_o = ((dataset['C-C'] - dataset['F-C']).abs() - (dataset['OR-C'] - dataset['OL-C']).abs()).abs()
-        #varg = (pd.DataFrame(dataset, columns=['RC-G', 'LC-G', 'C-G', 'F-G', 'OR-G', 'OL-G']).rolling(10).mean()).var(axis=1)
-        #varc = (pd.DataFrame(dataset, columns=['RC-C', 'LC-C', 'C-C', 'F-C', 'OR-C', 'OL-C']).rolling(10).mean()).var(axis=1)
-        #varg_m = varg.rolling(10).mean()
-        #varc_m = varc.rolling(10).mean()
+        difg_LCRC = (self.dataset['RC-G'] - self.dataset['LC-G']).abs()
+        difc_LCRC = (self.dataset['RC-C'] - self.dataset['LC-C']).abs()
+        difg_LCRC.iloc[:200].to_csv(self.dir + '/difgLR.csv')
+        difc_LCRC.iloc[:200].to_csv(self.dir + '/difcLR.csv')
+        difg_o = ((self.dataset['C-G'] - self.dataset['F-G']).abs() - (self.dataset['OR-G'] - self.dataset['OL-G']).abs()).abs()
+        difc_o = ((self.dataset['C-C'] - self.dataset['F-C']).abs() - (self.dataset['OR-C'] - self.dataset['OL-C']).abs()).abs()
+        difg_o.iloc[:200].to_csv(self.dir + '/difg_o.csv')
+        difc_o.iloc[:200].to_csv(self.dir + '/difc_o.csv')
+        derived_data = difg_LCRC.iloc[:200].append(difc_LCRC.iloc[:200], ignore_index=True).append(difg_o.iloc[:200], ignore_index=True).append(
+            difc_o.iloc[:200], ignore_index=True)
+
         if 'fake' in self.full_path.lower():
-            derived_data = difg_LCRC.iloc[:100].append(difc_LCRC.iloc[:100]).append(difg_o.iloc[:100]).append(difc_o.iloc[:100]).append(pd.Series(data=['FAKE']))
+            self.fake_data[self.fake_count] = derived_data
+            self.fake_count += 1
         else:
-            derived_data = difg_LCRC.iloc[:100].append(difc_LCRC.iloc[:100]).append(difg_o.iloc[:100]).append(difc_o.iloc[:100]).append(pd.Series(data=['REAL']))
+            self.real_data[self.real_count] = derived_data
+            self.real_count += 1
 
-        self.data['N' + str(self.vid_count)] = derived_data
+        self.vid_count += 1
 
 
     def main_loop(self, video_obj):
@@ -100,18 +103,19 @@ class signalWriter(object):
                 self.missed_frame_check = False
                 self.current_video_status = False
                 video_obj.stop()
-                dataset = pd.DataFrame.from_records(self.video_data)
-                self.write_derived_features(dataset)
+                self.dataset = pd.DataFrame.from_records(self.video_data)
+                self.write_derived_features()
                 if 'fake' in self.full_path.lower():
-                    dataset['Target'] = 0
+                    self.dataset['Target'] = 0
                 else:
-                    dataset['Target'] = 1
+                    self.dataset['Target'] = 1
 
                 processed_file_dir = Path("./processed" + self.full_path[1:self.full_path.rindex('/')])
                 processed_file_dir.mkdir(parents=True, exist_ok=True)
                 self.outputPath = str(processed_file_dir) + self.full_path[self.full_path.rindex('/'):-4] + ".csv"
-                dataset.to_csv(self.outputPath, index=False)
+                self.dataset.to_csv(self.outputPath, index=False)
                 self.outputPath = ''
+                self.video_data = []
                 return
 
         except Exception as excep:
@@ -143,10 +147,16 @@ class signalWriter(object):
                     except Exception as excep:
                         print("Exception occurred on file " + file + ": " + str(excep))
 
-        data_frame = pd.DataFrame(self.data).transpose()
-        test, train = train_test_split(data_frame, test_size=0.2)
-        train.to_csv(self.train_data_path, index=False)
-        test.to_csv(self.test_data_path, index=False)
+
+        self.real_df = pd.DataFrame(self.real_data)
+        self.fake_df = pd.DataFrame(self.fake_data)
+        r = self.real_df.transpose()
+        f = self.fake_df.transpose()
+        r['Target'] = 1
+        f['Target'] = 0
+        rf = r.append(f)
+        rf.to_csv(self.dir + '/all_data.csv', index=False)
+
 
 
 p = signalWriter()
